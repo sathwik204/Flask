7378df7bc65c5ce220b031c3139907ecce480450
sathwik204 <jangasathwikreddy04@gmail.com>
2025-03-25 12:32:49 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# app.py
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Load trained model
+model = joblib.load('iris_model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('home_html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from form
+    try:
+        features = [float(request.form[f'features{i}']) for i in range(1, 5)]
+    except ValueError:
+        return render_template('result.html', prediction="Invalid input. Please enter numeric values.")
+
+    # Make  prediction
+    prediction = model.predict([features])[0]
+
+    # Map prediction to class name
+    class_names = ['Setosa', 'Versicolor', 'Virginica']
+    result = class_names[prediction]
+
+    return render_template
+
+
+# In[ ]:
+
+
+
+
